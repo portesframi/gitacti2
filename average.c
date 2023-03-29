0b47807949c35272cfb76dd93ffa6cfc09d44334
portesframi <portesframi@gmail.com>
2023-03-29 22:10:19 +0200

    Añado average.c version 1

---
+++ average.c
@@ -0,0 +1,26 @@
+    #include <stdio.h>
+    #define N 1000
+
+    int num[N];
+
+    float func_average() {
+        int i;
+        float sum = 0.0;
+        for(i = 0; i < N; ++i)
+        {
+   	   sum += num[i];
+        }
+        return sum / N;
+    }
+
+    int main() {
+       int i;
+       float average;
+       for (i=0;i<N;i++) 
+         {
+             num[i]=i;
+       	 }
+        average = func_average();
+        printf("Average = %f\n", average);
+        return 0;
+    }
